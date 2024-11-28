993dd95a088090c3a39102c75d5260f3b127a466
Retno <hapsari.eno003@gmail.com>
2024-11-29 00:41:31 +0700

    yes

--- src/components/layouts/previewElement.js
+++ src/components/layouts/previewElement.js
@@ -4,34 +4,49 @@ import React from "react";
 
 // Template tata letak dasar
 const baseTemplate = [
-  { type: "flower", id: 5, top: "20px", left: "80px", zIndex: 2 },
-  { type: "decoration", id: 1, top: "40px", left: "120px", zIndex: 1 },
-  { type: "flower", id: 4, top: "20px", left: "160px", zIndex: 2 },
-  { type: "decoration", id: 2, top: "60px", left: "100px", zIndex: 1 },
-  { type: "flower", id: 3, top: "80px", left: "120px", zIndex: 2 },
-  { type: "decoration", id: 3, top: "100px", left: "60px", zIndex: 1 },
-  { type: "flower", id: 2, top: "140px", left: "80px", zIndex: 2 },
-  { type: "decoration", id: 4, top: "120px", left: "160px", zIndex: 1 },
-  { type: "flower", id: 1, top: "140px", left: "160px", zIndex: 2 },
+  { type: "flower", id: 5, top: "400px", left: "240px", zIndex: 4 },
+  { type: "flower", id: 4, top: "370px", left: "300px", zIndex: 3 },
+  { type: "flower", id: 3, top: "405px", left: "360px", zIndex: 3 },
+  { type: "flower", id: 2, top: "320px", left: "350px", zIndex: 2 },
+  { type: "flower", id: 1, top: "320px", left: "250px", zIndex: 2 },
+  { type: "flower", id: 9, top: "350px", left: "190px", zIndex: 2 },
+  { type: "flower", id: 6, top: "440px", left: "300px", zIndex: 4 },
+  { type: "flower", id: 8, top: "350px", left: "420px", zIndex: 2 },
+  { type: "flower", id: 7, top: "270px", left: "300px", zIndex: 2 },
+  { type: "decoration", id: 1, top: "315px", left: "195px", zIndex: 3 },
+  { type: "decoration", id: 2, top: "360px", left: "250px", zIndex: 3 },
+  { type: "decoration", id: 3, top: "320px", left: "310px", zIndex: 2 },
+  { type: "decoration", id: 4, top: "280px", left: "250px", zIndex: 2 },
+  { type: "decoration", id: 5, top: "340px", left: "140px", zIndex: 2 },
+  { type: "decoration", id: 6, top: "360px", left: "360px", zIndex: 2 },
+  { type: "decoration", id: 7, top: "230px", left: "190px", zIndex: 1 },
+  { type: "decoration", id: 8, top: "230px", left: "320px", zIndex: 1 },
 ];
 
 // Elemen tambahan untuk ukuran yang lebih besar
 const sizeAdditions = {
   medium: [
-    { type: "decoration", id: 5, top: "20px", left: "40px", zIndex: 1 },
-    { type: "flower", id: 9, top: "20px", left: "120px", zIndex: 2 },
-    { type: "decoration", id: 6, top: "20px", left: "200px", zIndex: 1 },
-    { type: "flower", id: 6, top: "100px", left: "40px", zIndex: 2 },
-    { type: "flower", id: 8, top: "100px", left: "200px", zIndex: 2 },
-    { type: "flower", id: 7, top: "180px", left: "120px", zIndex: 2 },
+    { type: "flower", id: 12, top: "260px", left: "180px", zIndex: 1 },
+    { type: "flower", id: 11, top: "220px", left: "240px", zIndex: 1 },
+    { type: "flower", id: 13, top: "230px", left: "370px", zIndex: 1 },
+    { type: "flower", id: 10, top: "280px", left: "450px", zIndex: 1 },
+    { type: "decoration", id: 9, top: "260px", left: "130px", zIndex: 1 },
+    { type: "decoration", id: 10, top: "180px", left: "250px", zIndex: 1 },
+    { type: "decoration", id: 11, top: "270px", left: "380px", zIndex: 1 },
   ],
   large: [
-    { type: "flower", id: 12, top: "0px", left: "60px", zIndex: 2 },
-    { type: "decoration", id: 7, top: "0px", left: "100px", zIndex: 1 },
-    { type: "flower", id: 11, top: "0px", left: "140px", zIndex: 2 },
-    { type: "flower", id: 13, top: "60px", left: "40px", zIndex: 2 },
-    { type: "decoration", id: 8, top: "60px", left: "200px", zIndex: 1 },
-    { type: "flower", id: 10, top: "100px", left: "220px", zIndex: 2 },
+    { type: "flower", id: 14, top: "380px", left: "120px", zIndex: 3 },
+    { type: "flower", id: 15, top: "300px", left: "100px", zIndex: 2 },
+    { type: "flower", id: 16, top: "210px", left: "120px", zIndex: 1 },
+    { type: "flower", id: 17, top: "170px", left: "320px", zIndex: 2 },
+    { type: "flower", id: 18, top: "200px", left: "450px", zIndex: 0 },
+    { type: "flower", id: 19, top: "340px", left: "500px", zIndex: 2 },
+    { type: "flower", id: 20, top: "420px", left: "460px", zIndex: 3 },
+    { type: "decoration", id: 12, top: "290px", left: "90px", zIndex: 2 },
+    { type: "decoration", id: 13, top: "220px", left: "80px", zIndex: 1 },
+    { type: "decoration", id: 14, top: "140px", left: "340px", zIndex: 0 },
+    { type: "decoration", id: 15, top: "200px", left: "380px", zIndex: 0 },
+    { type: "decoration", id: 16, top: "330px", left: "420px", zIndex: 2 },
   ],
 };
 
@@ -62,13 +77,13 @@ export default function PreviewElement({
   const layout = generateTemplate(size);
 
   return (
-    <div className="relative w-[400px] h-[400px] mx-auto bg-gray-100 rounded-md shadow-md">
+    <div className="relative w-[600px] h-screen mx-auto border">
       {/* Wrap Style */}
       {selectedWrap && (
         <img
           src={selectedWrap.wrap_image || "/images/placeholder.jpg"}
           alt="Wrap Style"
-          className="absolute inset-0 w-full h-full object-cover z-0"
+          className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-3/4 object-cover z-0"
         />
       )}
 
@@ -77,62 +92,63 @@ export default function PreviewElement({
         .filter((item) => item.type === "flower")
         .map((flowerPosition, index) => {
           const flower = selectedFlowers[index];
-          return flower ? (
+          return (
             <div
               key={`flower-${index}`}
-              className="absolute"
+              className="absolute flex justify-center items-center text-center"
               style={{
                 top: flowerPosition.top,
                 left: flowerPosition.left,
                 zIndex: flowerPosition.zIndex,
+                transform: "translate(-50%, -50%)", // Pastikan bunga terpusat
               }}
             >
-              <img
-                src={flower.flower_image || "/images/placeholder.jpg"}
-                alt={`Flower ${index + 1}`}
-                className="w-10 h-10 object-cover rounded-full"
-              />
+              {flower ? (
+                <img
+                  src={flower.flower_image || "/images/placeholder.jpg"}
+                  alt={`Flower ${index + 1}`}
+                  className="w-[80px] h-[80px] object-cover rounded-full"
+                />
+              ) : (
+                <span className="text-sm font-bold text-gray-500 flex justify-center items-center w-10 h-10 rounded-full border-2 border-dashed border-gray-400">
+                  {index + 1}
+                </span>
+              )}
             </div>
-          ) : null;
+          );
         })}
 
       {/* Decorations */}
-      {selectedDecoration &&
-        layout
-          .filter((item) => item.type === "decoration")
-          .map((decoPosition, index) => (
-            <div
-              key={`decoration-${index}`}
-              className="absolute"
-              style={{
-                top: decoPosition.top,
-                left: decoPosition.left,
-                zIndex: decoPosition.zIndex,
-              }}
-            >
-              <img
-                src={selectedDecoration.decoration_image}
-                alt="Decoration"
-                className="w-8 h-8 object-cover"
-              />
-            </div>
-          ))}
+      {layout
+        .filter((item) => item.type === "decoration")
+        .map(
+          (decoPosition, index) =>
+            selectedDecoration && (
+              <div
+                key={`decoration-${index}`}
+                className="absolute"
+                style={{
+                  top: decoPosition.top,
+                  left: decoPosition.left,
+                  zIndex: decoPosition.zIndex,
+                }}
+              >
+                <img
+                  src={selectedDecoration.decoration_image}
+                  alt="Decoration"
+                  className="w-[100px] h-[100px] object-cover"
+                />
+              </div>
+            )
+        )}
 
       {/* Ribbon */}
       {selectedRibbon && (
-        <div
-          className="absolute"
-          style={{
-            top: "300px",
-            left: "50%",
-            transform: "translateX(-50%)",
-            zIndex: 3,
-          }}
-        >
+        <div className="absolute bottom-[2%] left-1/2 transform -translate-x-1/2 z-10 flex justify-center items-center">
           <img
-            src={selectedRibbon.ribbon_image || "/images/placeholder.jpg"}
+            src={selectedRibbon?.ribbon_image || "/images/placeholder.jpg"}
             alt="Ribbon"
-            className="w-32 h-10 object-cover rounded-md"
+            className="w-1/2 h-auto object-cover rounded-md"
           />
         </div>
       )}
