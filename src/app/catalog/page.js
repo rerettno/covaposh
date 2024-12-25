80927d77f933ed85d4def5771254ec6a85433312
Retno <hapsari.eno003@gmail.com>
2024-12-25 22:58:17 +0700

    bisaplis

--- src/app/catalog/page.js
+++ src/app/catalog/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { Suspense, useState, useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import AddElementCustom from "src/components/form/addElementCustom";
 import AllProduct from "src/components/layouts/allProduct";
@@ -33,35 +33,37 @@ export default function CatalogPage() {
   };
 
   return (
-    <div>
-      <Navbar />
-      <div className="relative mt-[70px] h-[20px] md:h-[60px]">
-        <div
-          className="absolute inset-0 bg-repeat-x bg-center"
-          style={{
-            backgroundImage: "url('/images/divider.png')",
-            backgroundSize: "contain",
-          }}
-        ></div>
-        <div className="absolute inset-0 bg-blue/50"></div>
-      </div>
-
-      <div className="mt-7 grid grid-cols-1 lg:grid-cols-5 gap-4 h-[calc(100vh-50px)]">
-        {/* Sidebar filter section */}
-        <div className="lg:col-span-1 h-full">
-          <Filter
-            onFilterChange={handleFilterChange}
-            selectedCategory={filters.category}
-          />
+    <Suspense fallback={<p>Loading catalog...</p>}>
+      <div>
+        <Navbar />
+        <div className="relative mt-[70px] h-[20px] md:h-[60px]">
+          <div
+            className="absolute inset-0 bg-repeat-x bg-center"
+            style={{
+              backgroundImage: "url('/images/divider.png')",
+              backgroundSize: "contain",
+            }}
+          ></div>
+          <div className="absolute inset-0 bg-blue/50"></div>
         </div>
 
-        {/* Scrollable product section */}
-        <div className="lg:col-span-4 h-full overflow-y-scroll scrollbar-hide">
-          <AllProduct filters={filters} />
+        <div className="mt-7 grid grid-cols-1 lg:grid-cols-5 gap-4 h-[calc(100vh-50px)]">
+          {/* Sidebar filter section */}
+          <div className="lg:col-span-1 h-full">
+            <Filter
+              onFilterChange={handleFilterChange}
+              selectedCategory={filters.category}
+            />
+          </div>
+
+          {/* Scrollable product section */}
+          <div className="lg:col-span-4 h-full overflow-y-scroll scrollbar-hide">
+            <AllProduct filters={filters} />
+          </div>
         </div>
+        {/* <AddElementCustom /> */}
+        <Footer type="custom" />
       </div>
-      {/* <AddElementCustom /> */}
-      <Footer type="custom" />
-    </div>
+    </Suspense>
   );
 }
